67b36a5dcf79efeccc66b05dd8337daf86c7c2d7
mdatikHasan <atikh2030@gmail.com>
2022-07-19 16:48:07 +0600

    firebase

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import {  Route, BrowserRouter, Routes } from 'react-router-dom';
+import { Route, BrowserRouter, Routes } from 'react-router-dom';
 import './App.css';
 import Header from './components/Header/Header';
 import Inventory from './components/Inventory/Inventory';
@@ -8,51 +8,25 @@ import OrderReview from './components/OrderReview/OrderReview';
 import PlaceOrder from './components/PlaceOrder/PlaceOrder';
 import Register from './components/Register/Register';
 import Shop from './components/Shop/Shop';
+import AuthProvider from './context/AuthProvider';
 
 function App() {
   return (
     <div>
-      <BrowserRouter>
-        <Header></Header>
-        <Routes>
-          <Route path='/' element={<Shop></Shop>}></Route>
-          <Route path='/review' element={<OrderReview></OrderReview>}></Route>
-          <Route path='/inventory' element={<Inventory></Inventory>}></Route>
-          <Route path='/placeorder' element={<PlaceOrder></PlaceOrder>}></Route>
-          <Route path='/login' element={<Login></Login>}></Route>
-          <Route path='/register' element={<Register></Register>}></Route>
-          <Route path='*' element={<NotFound></NotFound>}></Route>
-
-        </Routes>
-      </BrowserRouter>
-
-      {/* <Router>
-        <Header></Header>
-        <Switch>
-          <Route exact path="/">
-            <Shop></Shop>
-          </Route>
-          <Route path="/shop">
-            <Shop></Shop>
-          </Route>
-          <Route path="/review">
-            <OrderReview></OrderReview>
-          </Route>
-          <Route path="/inventory">
-            <Inventory></Inventory>
-          </Route>
-          <Route path="/placeorder">
-            <PlaceOrder></PlaceOrder>
-          </Route>
-          <Route path='/login'>
-
-          </Route>
-          <Route path="*">
-            <NotFound></NotFound>
-          </Route>
-        </Switch>
-      </Router> */}
-
+      <AuthProvider>
+        <BrowserRouter>
+          <Header></Header>
+          <Routes>
+            <Route path='/' element={<Shop></Shop>}></Route>
+            <Route path='/review' element={<OrderReview></OrderReview>}></Route>
+            <Route path='/inventory' element={<Inventory></Inventory>}></Route>
+            <Route path='/placeorder' element={<PlaceOrder></PlaceOrder>}></Route>
+            <Route path='/login' element={<Login></Login>}></Route>
+            <Route path='/register' element={<Register></Register>}></Route>
+            <Route path='*' element={<NotFound></NotFound>}></Route>
+          </Routes>
+        </BrowserRouter>
+      </AuthProvider>
     </div>
   );
 }
--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,24 +1,25 @@
 import React from 'react';
 import { NavLink } from 'react-router-dom';
-import useFirebase from '../../hooks/useFirebase';
+import useAuth from '../../hooks/useAuth';
 import logo from '../../images/logo.png';
 import './Header.css';
 
 const Header = () => {
-    const {user, logOut} = useFirebase()
+    const { user, logOut } = useAuth()
     return (
         <div className="header">
             <img className="logo" src={logo} alt="" />
             <nav>
                 <NavLink to="/">Shop</NavLink>
                 <NavLink to="/review">Order Review</NavLink>
-                <NavLink to="/inventory">Manage Inventory</NavLink> 
-                <NavLink to="/register">Register</NavLink> 
+                <NavLink to="/inventory">Manage Inventory</NavLink>
+                <NavLink to="/register">Register</NavLink>
                 {
-                    user.email? 
-                    <button onClick={logOut}>Log Out</button>
-                    :
-                    <NavLink to="/login">Login</NavLink> }
+                    user.email ?
+                        <button onClick={logOut}>Log Out</button>
+                        :
+                        <NavLink to="/login">Login</NavLink>};
+                {user.email && <span className='user-name'>Hello! {user.displayName}</span>}
             </nav>
         </div>
     );
